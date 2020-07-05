be9462ca69143b2090e232aab35d250d2a141536
Benji <daylightsavings@gmail.com>
2020-07-04 23:58:40 -0400

    switch to prisma migrate

--- app/type_defs/policy_directive.ts
+++ app/type_defs/policy_directive.ts
@@ -7,8 +7,8 @@ class PolicyDirective extends SchemaDirectiveVisitor {
     const resolver = field.resolve || defaultFieldResolver
 
     field.resolve = function (...args) {
-      const { user } = args[2] || { user: { roles: [] } }
-      const authorized = user.admin || user.roles.map(i => i.name).includes(requires)
+      const { user } = args[2]
+      const authorized = user?.admin || user?.roles?.map(i => i.name)?.includes(requires)
       if (!authorized) throw new Error(`Not Authorized: ${requires}`)
 
       return resolver.apply(this, args)
